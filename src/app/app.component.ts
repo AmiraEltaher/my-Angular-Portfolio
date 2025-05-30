1b400b94b278dcc4bf7dca5cd313cc32b0dcf09e
AmiraEltaher <amiraeltaher2020@gmail.com>
2025-05-30 08:55:24 +0300

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,AboutComponent,ProjectsComponent],
+  imports: [RouterOutlet, NavbarComponent,AboutComponent,ProjectsComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
