ac73195655326bf9d74af9ffededcc8fc6563e41
AmiraEltaher <amiraeltaher2020@gmail.com>
2025-05-30 08:34:19 +0300

    nabBar and About

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration()],
+
 };
