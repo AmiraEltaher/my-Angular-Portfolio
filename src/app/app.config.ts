25787c46454d89f729b28b63297b9e8228c86efc
AmiraEltaher <amiraeltaher2020@gmail.com>
2025-05-30 08:43:55 +0300

    projects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
-import { NavbarComponent } from './navbar/navbar.component';
-import { AboutComponent } from './about/about.component';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()],
